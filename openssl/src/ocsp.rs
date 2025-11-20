f2a461c1f4f76f2cfea18080ff759217da53146b
Dan Pashin <danypashin@yandex.ru>
2025-11-20 19:28:35 +0300

    Fix invalid value parsing of OCSP revocation reason

--- openssl/src/ocsp.rs
+++ openssl/src/ocsp.rs
@@ -237,7 +237,7 @@ impl OcspBasicResponseRef {
                 #[allow(deprecated)]
                 Some(OcspStatus {
                     status: OcspCertStatus(status),
-                    reason: OcspRevokedStatus(status),
+                    reason: OcspRevokedStatus(reason),
                     revocation_time,
                     this_update: Asn1GeneralizedTimeRef::from_ptr(this_update),
                     next_update: next_update_compat,
@@ -394,6 +394,7 @@ foreign_type_and_impl_send_sync! {
 mod tests {
     use super::{
         get_sentinel_max_time, OcspCertId, OcspCertStatus, OcspResponse, OcspResponseStatus,
+        OcspRevokedStatus,
     };
     use crate::hash::MessageDigest;
     use crate::x509::X509;
@@ -403,6 +404,7 @@ mod tests {
         include_bytes!("../test/ocsp_resp_no_nextupdate.der");
     const OCSP_CA_CERT: &[u8] = include_bytes!("../test/ocsp_ca_cert.der");
     const OCSP_SUBJECT_CERT: &[u8] = include_bytes!("../test/ocsp_subject_cert.der");
+    const OCSP_RESPONSE_REVOKED: &[u8] = include_bytes!("../test/ocsp_resp_revoked.der");
 
     #[test]
     fn test_ocsp_no_next_update() {
@@ -429,5 +431,25 @@ mod tests {
         assert_eq!(format!("{}", deprecated_next), format!("{}", sentinel));
 
         assert_eq!(status.status, OcspCertStatus::GOOD);
+    }
+
+    #[test]
+    fn test_ocsp_revoked() {
+        let response = OcspResponse::from_der(OCSP_RESPONSE_REVOKED).unwrap();
+        assert_eq!(response.status(), OcspResponseStatus::SUCCESSFUL);
+
+        let ca_cert = X509::from_der(OCSP_CA_CERT).unwrap();
+        let subject_cert = X509::from_der(OCSP_SUBJECT_CERT).unwrap();
+        let basic = response.basic().unwrap();
+
+        let cert_id =
+            OcspCertId::from_cert(MessageDigest::sha256(), &subject_cert, &ca_cert).unwrap();
+
+        let status = basic
+            .find_status(&cert_id)
+            .expect("find_status should find the status");
+
+        assert_eq!(status.status, OcspCertStatus::REVOKED);
+        assert_eq!(status.reason, OcspRevokedStatus::STATUS_SUPERSEDED);
     }
 }
